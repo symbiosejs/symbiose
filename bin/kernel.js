4df4cce1608005ab7e7b83bc9a17b20538336f0e
A-312 <A-312@users.noreply.github.com>
2019-08-16 20:25:05 +0200

    Symbiont loader

--- bin/kernel.js
+++ bin/kernel.js
@@ -1,7 +1,6 @@
 const debug = require('debug')('symbiose:kernel')
 const EventEmitter = require('./event/bin/index.js');
 const fs = require('fs')
-const createRequire = require('module').createRequire || require('module').createRequireFromPath
 const path = require('path')
 const objectAssign = require('object-assign')
 
@@ -10,6 +9,11 @@ const App = require('./app-express.js')
 
 const configLoader = require('./convict/configloader.js')
 const Symbiont = require('symbiont')
+
+/* require in parent directory work context */
+const createRequire = require('module').createRequire || require('module').createRequireFromPath
+const requireSymbiont = createRequire('./')
+
 
 /**
  * Kernel of Symbiose
@@ -24,60 +28,47 @@ function Kernel(arg) {
    * @name config
    * @memberOf Kernel
    */
-  Object.defineProperty(Kernel.prototype, "config", {
+  Object.defineProperty(this, "config", {
     value: new configLoader(arg.config)
+  })
+
+  /**
+   * Event Emitter
+   *
+   * @name eventEmitter
+   * @memberOf Kernel
+   */
+  Object.defineProperty(this, "eventEmitter", {
+    value: new EventEmitter()
   })
 
   if (arg.symbionts && arg.symbionts instanceof Array) {
     debug('loading symbionts...')
 
-    const requireSymbiont = createRequire('./')
-
-    const ErrorWithSymbiont = function (message) {
-      debug(message)
-      return new Error(message)
-    }
-
-    arg.symbionts.forEach((symbiontName, key) => {
-      // symbiontName must be a string
-      if (typeof symbiontName !== "string") {
-        const message = `ERROR: Invalid parameter for new Kernel(arg), arg.symbionts[${key}] should be a string`
-        throw new ErrorWithSymbiont(message)
-      }
-
-      debug(`loading '${symbiontName}'...`)
-
-      // symbiont (module) must be found
-      const symbiont = (() => {
-        try {
-          return requireSymbiont(symbiontName)
-        } catch (err) {
-          const message = `ERROR: Invalid parameter for new Kernel(arg.symbionts[${key}]), cannot find module '${symbiontName}'`
-          throw new ErrorWithSymbiont(message)
-        }
-      })()
-
-      // module must be a symbiont
-      if (symbiont.constructor.name !== Symbiont.name ||
-        symbiont.constructorNameString !== Symbiont.name ||
-        typeof symbiont.versionSymbiontConstructor !== "string") {
-        const message = `ERROR: Invalid parameter for new Kernel(arg.symbionts[${key}]), module '${symbiontName}' is not a symbiont`
-        throw new ErrorWithSymbiont(message)
-      }
-
-      console.log(symbiont.getPrivateStorage().controllers.home && symbiont.getPrivateStorage().controllers.home.test())
-    })
+    arg.symbionts.forEach((val, key) => symbiontLoader.call(this, val, key))
   } else {
     debug('No symbiont registered to load with Symbiose, check `new Kernel(arg.symbionts)`.')
     //TODO: Fire an error.
   }
 }
 
+
 module.exports = Kernel
 
 
 Object.defineProperty(Kernel, "env", {
   value: getCurrentEnv()
+})
+
+
+/**
+ * Symbionts { <symbiontName>: <symbiontClass> }
+ *
+ * @name symbionts
+ * @memberOf Kernel
+ */
+Object.defineProperty(Kernel.prototype, "symbionts", {
+  value: {}
 })
 
 
@@ -123,4 +114,49 @@ function getCurrentEnv() {
   }
 
   return env
+}
+
+
+function symbiontLoader(symbiontName, key) {
+  const ErrorWithSymbiont = function(message) {
+    debug(message)
+    return new Error(message)
+  }
+
+  // symbiontName must be a string
+  if (typeof symbiontName !== "string") {
+    const message = `ERROR: Invalid parameter for arg.symbionts[${key}] in new Kernel(arg) should be a string`
+    throw new ErrorWithSymbiont(message)
+  }
+
+  debug(`loading '${symbiontName}'...`)
+
+  // symbiont (module) must be found
+  const symbiont = (() => {
+    try {
+      return requireSymbiont(symbiontName)
+    } catch (err) {
+      const message = `ERROR: Invalid parameter for arg.symbionts[${key}]) in new Kernel(arg) cannot find module '${symbiontName}'`
+      throw new ErrorWithSymbiont(message)
+    }
+  })()
+
+  // module must be a symbiont
+  if (symbiont.constructor.name !== Symbiont.name ||
+    symbiont.constructorNameString !== Symbiont.name ||
+    typeof symbiont.versionSymbiontConstructor !== "string") {
+    const message = `ERROR: Invalid parameter for new Kernel(arg.symbionts[${key}]), module '${symbiontName}' is not a symbiont`
+    throw new ErrorWithSymbiont(message)
+  }
+
+  // module must be a symbiont
+  if (this.symbionts[key]) {
+    const message = `ERROR: arg.symbionts[${key}] is already defined, you can't rewrite`
+    throw new ErrorWithSymbiont(message)
+  }
+
+  /* Only store controllers */
+  Object.defineProperty(this.symbionts, key, {
+    value: symbiont.getPrivateStorage().controllers
+  })
 }
