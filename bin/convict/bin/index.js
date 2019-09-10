24eff9d633a6a71110a078ea300a70db483f40ac
A-312 <A-312@users.noreply.github.com>
2019-09-10 07:28:33 +0200

    Add routes loader

--- bin/convict/bin/index.js
+++ bin/convict/bin/index.js
@@ -6,13 +6,28 @@ convict.addParser({ extension: ['yml', 'yaml'], parse: yaml.parse });
 convict.addFormat({
   name: 'source-array',
   validate: function(sources, schema) {
-    if (!Array.isArray(sources)) {
+    if (!sources instanceof Array) {
       throw new Error('must be of type Array');
     }
 
-    for (source of sources) {
+    sources.forEach((source) => {
       convict(schema.children).load(source).validate();
+    })
+  }
+});
+
+convict.addFormat({
+  name: 'array[String]',
+  validate: function(strings, schema) {
+    if (!Array.isArray(strings)) {
+      throw new Error('must be an Array');
     }
+
+    strings.forEach((string, key) => {
+      if (typeof string !== 'string') {
+        throw new TypeError(`Array [${key}] is not a string`);
+      }
+    })
   }
 });
 
---
+++ bin/convict/routesloader.js
@@ -0,0 +1,68 @@
+const debug = require('debug')('symbiose:configloader')
+const error = require('debug')('symbiose:configloader:error')
+const Convict = require('./bin/index.js')
+const path = require('path')
+const YAML = require('yaml')
+const fs = require('fs')
+
+/**
+ * Loader for `config.yml` files.
+ *
+ * @class
+ */
+function RoutesLoader(paths) {
+  debug('Start to preload routes:')
+
+  if (paths && typeof paths === "string") {
+    paths = [ paths ]
+  }
+
+  if (paths && (paths instanceof Array) === false) {
+    debug('`options.config` key is optionnal but if it declared should be an array or a string in: `new Kernal(options)`.')
+    process.exit(1)
+  }
+
+  if (!paths) {
+    debug('Symbiose will load on the default config because no config file is/are selected.')
+  }
+
+  const $$ = Convict(path.resolve(__dirname, 'schema', 'config.yml'))
+
+  /**
+   * Convict
+   *
+   * @name $$
+   * @memberOf RoutesLoader
+   */
+  Object.defineProperty(this, "$$", {
+    value: $$
+  })
+
+  paths.forEach((path) => {
+    debug(' - Loading %s', path)
+    $$.loadFile(path)
+  })
+
+  fs.mkdirSync(path.resolve($$.get('path.cache')), { recursive: true })
+  const data = new Uint8Array(Buffer.from(JSON.stringify($$.getProperties(), null, 2)))
+
+  const filepath = path.resolve($$.get('path.cache'), 'config.yml.json')
+  fs.writeFileSync(filepath, data)
+  debug('Cache file created: %s', filepath)
+
+  try {
+    $$.validate()
+  } catch (err) {
+    error('Check your configuration you have an invalid value :')
+    error(err.message)
+    process.exit(1)
+  }
+}
+
+
+RoutesLoader.prototype.load = function(app) {
+  
+}
+
+
+module.exports = RoutesLoader
