0374f47e078bc4104b265212f960c87fccede5fd
A-312 <A-312@users.noreply.github.com>
2019-09-10 06:39:02 +0200

    Add Symbionts loader

--- bin/kernel.js
+++ bin/kernel.js
@@ -1,5 +1,6 @@
 const debug = require('debug')('symbiose:kernel')
 const fs = require('fs')
+const createRequire = require('module').createRequire || require('module').createRequireFromPath
 const path = require('path')
 const objectAssign = require('object-assign')
 
@@ -7,6 +8,7 @@ const Http = require('./http.js')
 const App = require('./app-express.js')
 
 const configLoader = require('./../lib/convict/configloader.js')
+const Symbiont = require('symbiont')
 
 /**
  * Kernel of Symbiose
@@ -27,11 +29,46 @@ function Kernel(arg) {
 
   if (arg.symbionts && arg.symbionts instanceof Array) {
     debug('loading symbionts...')
-    arg.symbionts.forEach((symbiont) => {
-      loadSymbiont(symbiont)
+
+    const requireSymbiont = createRequire('./')
+
+    const ErrorWithSymbiont = function (message) {
+      debug(message)
+      return new Error(message)
+    }
+
+    arg.symbionts.forEach((symbiontName, key) => {
+      // symbiontName must be a string
+      if (typeof symbiontName !== "string") {
+        const message = `ERROR: Invalid parameter for new Kernel(arg), arg.symbionts[${key}] should be a string`
+        throw new ErrorWithSymbiont(message)
+      }
+
+      debug(`loading '${symbiontName}'...`)
+
+      // symbiont (module) must be found
+      const symbiont = (() => {
+        try {
+          return requireSymbiont(symbiontName)
+        } catch (err) {
+          const message = `ERROR: Invalid parameter for new Kernel(arg.symbionts[${key}]), cannot find module '${symbiontName}'`
+          throw new ErrorWithSymbiont(message)
+        }
+      })()
+
+      // module must be a symbiont
+      if (symbiont.constructor.name !== Symbiont.name ||
+        symbiont.constructorNameString !== Symbiont.name ||
+        typeof symbiont.versionSymbiontConstructor !== "string") {
+        const message = `ERROR: Invalid parameter for new Kernel(arg.symbionts[${key}]), module '${symbiontName}' is not a symbiont`
+        throw new ErrorWithSymbiont(message)
+      }
+
+      console.log(symbiont.getPrivateStorage().controllers.home && symbiont.getPrivateStorage().controllers.home.test())
     })
   } else {
-    require('debug')('symbiose:warn')('No symbiont registered to load with Symbiose, check `new Kernel(arg.symbionts)`.')
+    debug('No symbiont registered to load with Symbiose, check `new Kernel(arg.symbionts)`.')
+    //TODO: Fire an error.
   }
 }
 
@@ -63,11 +100,6 @@ Kernel.prototype.launchServer = function() {
 
 Kernel.prototype.start = function(envName, filename) {
   this.launchServer()
-}
-
-
-function loadSymbiont(packageName) {
-  console.log(packageName)
 }
 
 
